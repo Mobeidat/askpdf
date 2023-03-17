0ae200b7fe5b262cbb023e01817562fd34b76a57
mobeidat <moathobiedat@hotmail.com>
2023-03-17 21:45:05 +0400

    enable on all routes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
